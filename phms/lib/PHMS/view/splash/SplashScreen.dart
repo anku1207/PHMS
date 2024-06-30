98ea6a922f8554a18c7ac7217bdb456346882acd
anku1207 <manojshakya1207@gmail.com>
2024-06-30 22:08:18 +0530

    add location permission

--- phms/lib/PHMS/view/splash/SplashScreen.dart
+++ phms/lib/PHMS/view/splash/SplashScreen.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:lottie/lottie.dart';
 import 'package:phms/PHMS/components/Session.dart';
+import 'package:phms/PHMS/components/UiUtility.dart';
 import 'package:phms/PHMS/components/constants.dart';
 import 'package:phms/PHMS/components/routes.dart';
+import 'package:phms/PHMS/components/utility.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashView extends StatefulWidget {
   @override
@@ -36,22 +41,108 @@ class _SplashViewState extends State<SplashView> with SingleTickerProviderStateM
           (Timer timer) {
         if (_start == 0) {
           _timer.cancel();
-          checkCustomerSession().then((value) {
-            if (value != null) {
-              Navigator.pushNamedAndRemoveUntil(
-                context,
-                UavRoutes.Home_Screen,
-                    (route) => false,
-              );
-            }else{
-              Navigator.pushReplacementNamed(context, UavRoutes.Login_Screen);
-            }
-          });
+          getLocation();
+          //_startApplication();
+
+
         } else {
           _start--;
         }
       },
     );
+  }
+
+  void getLocation() {
+    _determinePosition().catchError(
+          (onError) async {
+        print(onError.toString());
+        showToastShortTime(context, onError.toString());
+        showPermissionDialog(onError.toString());
+      },
+    ).then((value) async {
+      if (value != null) {
+        print(value.latitude.toString() + "===" + value.longitude.toString());
+        //setting the details in session manager
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        prefs.setString(CACHE_LOCATION_LAT, value.latitude.toString());
+        prefs.setString(CACHE_LOCATION_LON, value.longitude.toString());
+
+        _startApplication();
+      } else {
+        showSnackBar(context, "else");
+      }
+    }).whenComplete(() {
+      EasyLoading.dismiss();
+      print("called when future completes");
+    });
+  }
+
+  Future<Position?> _determinePosition() async {
+    EasyLoading.show(status: 'wait...');
+
+    bool serviceEnabled;
+    LocationPermission permission;
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await Geolocator.openLocationSettings();
+      showToastShortTime(context, "serviceEnabled");
+      if (!serviceEnabled) {
+        return Future.error('Location services are disabled.');
+      }
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission().catchError((onError) {
+        showToastShortTime(context, onError.toString());
+      });
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+  showPermissionDialog(String string) async {
+    String? dialogResp = await showAlertDialog(
+        context: context,
+        btnNameOk: "ok",
+        btnNameCancel: "cancel",
+        title: "Permission ",
+        message: "Need GPS permission for getting your location ");
+    if (dialogResp != null && dialogResp.toLowerCase() == "ok") {
+      getLocation();
+    } else {
+      SystemNavigator.pop();
+    }
+  }
+
+  _startApplication(){
+    checkCustomerSession().then((value) {
+      if (value != null) {
+        Navigator.pushNamedAndRemoveUntil(
+          context,
+          UavRoutes.Home_Screen,
+              (route) => false,
+        );
+      }else{
+        Navigator.pushReplacementNamed(context, UavRoutes.Login_Screen);
+      }
+    });
   }
 
 
